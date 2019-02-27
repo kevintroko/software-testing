a845356e1ed3504d52b7ecc5e627c69869ce75e2
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-02-27 01:29:38 -0600

    Finish changes to test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,20 @@ package com.company;
 /**
 * Find the last index of elements
 *
-* @param x array to search
-* @param y value to look for
+* param x array to search
+* param y value to look for
 * @return the last index of y in x; -1 if absent
 * @Throws NullPointerException if x is null
 */
 
 public class Main {
     public static int findLast (int[] x, int y) {
-        for(int i=x.length-1; i>0; i--){
+        for(int i=x.length-1; i>=0; i--){
             if(x[i]==y){
                 return i;
             }
         }
     return -1;
     }
+
 }
--- test/com/company/MainTest.java
+++ test/com/company/MainTest.java
@@ -6,7 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+    Main mn = new Main();
+    int[] xArray = {1,2,3,4};
+
     @Test
-    void findLast() {
+    void findLastFail() {
+        assertEquals(3, mn.findLast(xArray,4));
+    }
+
+    @Test
+    void findLastPass() {
+        assertEquals(0, mn.findLast(xArray,1));
+    }
+
+    @Test
+    void findLastOneDimension() {
+        int[] xOne = {0};
+        assertEquals(0, mn.findLast(xOne,0));
+    }
+
+    @Test
+    void question13() {
+        int[] x13 = {2,3,5};
+        assertEquals(0, mn.findLast(x13,2));
     }
 }
